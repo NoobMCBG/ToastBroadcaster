19ba00f28feaab4dbe8cdd7c89d27d7a575f11bc
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-28 13:33:31 +0700

    Add files

---
+++ src/NoobMCBG/ToastBroadcaster/ToastBroadcaster.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoobMCBG\ToastBroadcaster;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\network\mcpe\protocol\ToastRequestPacket;
+
+class ToastBroadcaster extends PluginBase {
+
+	public static $instance;
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
+
+	public function onEnable() : void {
+		$this->saveDefaultConfig();
+		$this->getScheduler()->scheduleRepeatingTask(new ToastBroadcasterTask($this), 20 * $this->getConfig()->getAll()["broadcast"]["delay"]);
+		self::$instance = $this;
+	}
+
+	public function sendPacket(Player $player, string $title = "", string $subtitle = ""){
+		$packet = ToastRequestPacket::create(
+        	$title,
+        	$subtitle
+		);
+		$player->getNetworkSession()->sendDataPacket($packet);
+	}
+
+	public function broadcastPacket(string $title = "", string $subtitle = ""){
+		foreach($this->getServer()->getOnlinePlayers() as $player){
+			$packet = ToastRequestPacket::create(
+        		$title,
+        		$subtitle
+			);
+			$player->getNetworkSession()->sendDataPacket($packet);
+		}
+	}
+}
