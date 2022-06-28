12cd119148c270a44af15c675fcb2acd9b2007bc
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-28 13:41:23 +0700

    Create ToastBroadcasterTask.php

---
+++ src/NoobMCBG/ToastBroadcaster/task/ToastBroadcasterTask.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NoobMCBG\ToastBroadcaster;
+
+use pocketmine\scheduler\Task;
+
+class ToastBroadcasterTask extends Task {
+    
+    	private ToastBroadcaster $plugin;
+    
+    	private $i;
+    
+    	public function __construct(ToastBroadcaster $plugin){
+        	$this->plugin = $plugin;
+		$this->i = 0;
+    	}
+
+    	public function onRun() : void {
+        	$title = $this->plugin->getConfig()->get("title");
+        	$messages = $this->plugin->getConfig()->getAll()["broadcast"]["message"];
+    		back:
+    		if($this->i < count($messages)){
+    	    		$this->plugin->broadcastToast($title, $messages[$this->i]);
+    	    		$this->i++;
+    		}else{
+		    	$this->i = 0;
+		    	goto back;
+		}
+    	}
+}
