59c5fa2b141db53f3403cb99232a043765b35895
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2023-01-25 14:45:07 +0700

    Update API
    4.0.0 -> 4.11.0

--- src/NoobMCBG/ToastBroadcaster/ToastBroadcaster.php
+++ src/NoobMCBG/ToastBroadcaster/ToastBroadcaster.php
@@ -6,7 +6,6 @@ namespace NoobMCBG\ToastBroadcaster;
 
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\network\mcpe\protocol\ToastRequestPacket;
 use NoobMCBG\ToastBroadcaster\task\ToastBroadcasterTask;
 use NoobMCBG\ToastBroadcaster\task\CheckUpdateTask;
 
@@ -14,36 +13,28 @@ class ToastBroadcaster extends PluginBase {
 
 	public static $instance;
 
-	public static function getInstance() : self {
+	public static function getInstance(): self {
 		return self::$instance;
 	}
 
-	public function onEnable() : void {
+	protected function onEnable(): void {
 		$this->saveDefaultConfig();
 		$this->checkUpdate();
 		$this->getScheduler()->scheduleRepeatingTask(new ToastBroadcasterTask($this), 20 * $this->getConfig()->getAll()["broadcast"]["delay"]);
 		self::$instance = $this;
 	}
-	
-	public function checkUpdate(bool $isRetry = false): void {
-            	$this->getServer()->getAsyncPool()->submitTask(new CheckUpdateTask($this->getDescription()->getName(), $this->getDescription()->getVersion()));
-        }
 
-	public function sendToast(Player $player, string $title = "", string $subtitle = ""){
-		$packet = ToastRequestPacket::create(
-        	$title,
-        	$subtitle
-		);
-		$player->getNetworkSession()->sendDataPacket($packet);
+	public function checkUpdate(bool $isRetry = false): void {
+		$this->getServer()->getAsyncPool()->submitTask(new CheckUpdateTask($this->getDescription()->getName(), $this->getDescription()->getVersion()));
 	}
 
-	public function broadcastToast(string $title = "", string $subtitle = ""){
-		foreach($this->getServer()->getOnlinePlayers() as $player){
-			$packet = ToastRequestPacket::create(
-        		$title,
-        		$subtitle
-			);
-			$player->getNetworkSession()->sendDataPacket($packet);
+	public function sendToast(Player $player, string $title = "", string $subtitle = ""): void {
+		$player->sendToastNotification($title, $subtitle);
+	}
+
+	public function broadcastToast(string $title = "", string $subtitle = ""): void {
+		foreach ($this->getServer()->getOnlinePlayers() as $player) {
+			$$player->sendToastNotification($title, $subtitle);
 		}
 	}
 }
--- src/NoobMCBG/ToastBroadcaster/task/ToastBroadcasterTask.php
+++ src/NoobMCBG/ToastBroadcaster/task/ToastBroadcasterTask.php
@@ -6,26 +6,26 @@ use pocketmine\scheduler\Task;
 use NoobMCBG\ToastBroadcaster\ToastBroadcaster;
 
 class ToastBroadcasterTask extends Task {
-    
-    	private ToastBroadcaster $plugin;
-    
-    	private $i;
-    
-    	public function __construct(ToastBroadcaster $plugin){
-        	$this->plugin = $plugin;
-		$this->i = 0;
-    	}
 
-    	public function onRun() : void {
-        	$title = $this->plugin->getConfig()->get("title");
-        	$messages = $this->plugin->getConfig()->getAll()["broadcast"]["message"];
-    		back:
-    		if($this->i < count($messages)){
-    	    		$this->plugin->broadcastToast($title, $messages[$this->i]);
-    	    		$this->i++;
-    		}else{
-		    	$this->i = 0;
-		    	goto back;
+	private ToastBroadcaster $plugin;
+
+	private int $i;
+
+	public function __construct(ToastBroadcaster $plugin) {
+		$this->plugin = $plugin;
+		$this->i = 0;
+	}
+
+	public function onRun(): void {
+		$title = $this->plugin->getConfig()->get("title");
+		$messages = $this->plugin->getConfig()->getAll()["broadcast"]["message"];
+		back:
+		if ($this->i < count($messages)) {
+			$this->plugin->broadcastToast($title, $messages[$this->i]);
+			$this->i++;
+		} else {
+			$this->i = 0;
+			goto back;
 		}
-    	}
+	}
 }
